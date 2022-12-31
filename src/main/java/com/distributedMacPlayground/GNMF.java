c0091eebcfe7ef2c82b49760fd3d3f34c2eff8a9
zerorains <me@zerorains.top>
2022-12-31 12:39:14 +0800

    finished to read the .mtx matrix

--- src/main/java/com/distributedMacPlayground/GNMF.java
+++ src/main/java/com/distributedMacPlayground/GNMF.java
@@ -5,15 +5,20 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.sysds.common.Types;
 import org.apache.sysds.runtime.controlprogram.caching.MatrixObject;
 import org.apache.sysds.runtime.matrix.data.MatrixBlock;
+import org.apache.sysds.runtime.meta.MatrixCharacteristics;
+import org.apache.sysds.runtime.meta.MetaDataFormat;
 
 import java.util.List;
 
 
 public class GNMF {
     public static void main(String[] args) {
-        String hadoopLocation = "hdfs://10.1.1.1:19000/user/root/";
+        String hadoopLocation = "hdfs://7d88ca6d3cf1:9000/user/root/";
         String path = hadoopLocation + "soc-buzznet.mtx";
-        MatrixObject mo = new MatrixObject(Types.ValueType.FP64,path);
+        MatrixObject mo = new MatrixObject(Types.ValueType.FP64, path);
+        MatrixCharacteristics mc = new MatrixCharacteristics(101163, 101163);
+        MetaDataFormat mdf = new MetaDataFormat(mc, Types.FileFormat.MM);
+        mo.setMetaData(mdf);
         MatrixBlock matrixBlock = mo.acquireRead();
         System.out.println(matrixBlock.getNumRows());
         System.out.println(matrixBlock.getNumColumns());
